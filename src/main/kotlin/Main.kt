1ae91a421f81b0cdc828b7f56263a84126f1ad0d
xhinodread <chileregion@gmail.com>
2022-07-10 15:09:48 -0400

    unicial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,55 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    // infix fun Int.times(str: String) = str.repeat(this)        // 1
+    // println(2 times "Bye ")                                    // 2
+
+    operator fun Int.times(str: String) = str.repeat(this)       // 1
+    println(2 * "Bye ")                                          // 2
+
+    operator fun String.get(range: IntRange) = substring(range)  // 3
+    val str = "Always forgive your enemies; nothing annoys them so much."
+    println(str[0..14])
+
+
+
+
+
+    val vecs = 7
+    infix fun Int.timesdos(str: String) = str.repeat(this)        // 1
+    println(vecs timesdos "chau... ")                                    // 2
+
+    val pair = "Ferrari" to "Katrina"                          // 3
+    println(pair)
+
+    infix fun String.onto(other: String) = Pair(this, other)   // 4
+    val myPair = "McLaren" onto "Lucas"
+    println(myPair)
+
+    val sophia = Person("Sophia")
+    val claudia = Person("Claudia")
+    val lainfix = sophia likes claudia                                       // 5
+    println(lainfix)
+
+    fun printAll(vararg messages: String) {                            // 1
+        for (m in messages) println(m)
+    }
+    printAll("Hello", "Hallo", "Salut", "Hola", "你好", "fin")                 // 2
+
+    fun printAllWithPrefix(vararg messages: String, prefix: String) {  // 3
+        for (m in messages) println(prefix + m)
+    }
+    printAllWithPrefix(
+        "Hello", "Hallo", "Salut", "Hola", "你好", prefix = "Greeting: "                                          // 4
+    )
+
+    fun log(vararg entries: String) {
+        printAll(*entries)                                             // 5
+    }
+
+}
+
+class Person(val name: String) {
+    val likedPeople = mutableListOf<Person>()
+    infix fun likes(other: Person) { likedPeople.add(other) }  // 6
+
 }
